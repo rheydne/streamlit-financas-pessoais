f2810ccf8ff83635779e0c120f57129e11538ca5
Rheydne Silva <rheydne@gmail.com>
2025-08-24 18:17:41 -0300

    feat: Implementação de Estatísticas Gerais

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
-
 def tratamento_csv_entrada(df):
     df['Valor'] = df['Valor'].str.replace('R$ ', '', regex=False)
     df['Valor'] = df['Valor'].str.replace('.', '', regex=False)
@@ -12,7 +10,30 @@ def tratamento_csv_entrada(df):
 
     return df
 
+def calc_general_stats(df):
+    df_data = df.groupby(by='Data')[['Valor']].sum()
+     
+    df_data['lag_1'] = df_data['Valor'].shift(1)
 
+    df_data['Diferença Mensal'] = df_data['Valor'] - df_data['lag_1']
+
+    df_data['Dif Mensal Média 6M'] = df_data['Diferença Mensal'].rolling(6).mean()
+    df_data['Dif Mensal Média 12M'] = df_data['Diferença Mensal'].rolling(12).mean()
+    df_data['Dif Mensal Média 24M'] = df_data['Diferença Mensal'].rolling(24).mean()
+
+    df_data['Dif Mensal Relativa'] = (df_data['Valor'] / df_data['lag_1'] - 1)
+
+    df_data['Evolução Total 6M'] = df_data['Valor'].rolling(6).apply(lambda x: x[-1] - x[0])
+    df_data['Evolução Total 12M'] = df_data['Valor'].rolling(12).apply(lambda x: x[-1] - x[0])
+    df_data['Evolução Total 24M'] = df_data['Valor'].rolling(24).apply(lambda x: x[-1] - x[0])
+     
+    df_data['Evolução Rel 6M'] = df_data['Valor'].rolling(6).apply(lambda x: x[-1] / x[0] - 1)
+    df_data['Evolução Rel 12M'] = df_data['Valor'].rolling(12).apply(lambda x: x[-1] / x[0] - 1)
+    df_data['Evolução Rel 24M'] = df_data['Valor'].rolling(24).apply(lambda x: x[-1] / x[0] - 1)
+
+    df_data = df_data.drop('lag_1', axis=1)
+
+    return df_data
 
 st.set_page_config(page_title='Finanças', page_icon=':classical_building:')
 
@@ -58,9 +79,50 @@ if file_upload:
 
     # Aba última data de dados
     with tab_share:
-
         # Filtro de data
         date = st.selectbox('Data para Distribuição', df_instituicao.index)
-
-        #Gráfico de barras, usando o filtro selecionado
+        # Gráfico de barras, usando o filtro selecionado
         st.bar_chart(df_instituicao.loc[date])
+
+    # Visão de Estatísticas Gerais
+    exp3 = st.expander('Estatísticas Gerais')
+
+    columns_fmt = {
+        'Valor':st.column_config.NumberColumn('Valor', format='R$ %.2f'),
+        'Diferença Mensal':st.column_config.NumberColumn('Diferença Mensal', format='R$ %.2f'),
+        'Dif Mensal Média 6M':st.column_config.NumberColumn('Dif Mensal Média 6M', format='R$ %.2f'),
+        'Dif Mensal Média 12M':st.column_config.NumberColumn('Dif Mensal Média 12M', format='R$ %.2f'),
+        'Dif Mensal Média 24M':st.column_config.NumberColumn('Dif Mensal Média 24M', format='R$ %.2f'),
+        'Dif Mensal Relativa':st.column_config.NumberColumn('Dif Mensal Relativa', format='percent'),
+        'Evolução Total 6M':st.column_config.NumberColumn('Evolução Total 6M', format='R$ %.2f'),
+        'Evolução Total 12M':st.column_config.NumberColumn('Evolução Total 12M', format='R$ %.2f'),
+        'Evolução Total 24M':st.column_config.NumberColumn('Evolução Total 24M', format='R$ %.2f'),
+        'Evolução Rel 6M':st.column_config.NumberColumn('Evolução Rel 6M', format='percent'),
+        'Evolução Rel 12M':st.column_config.NumberColumn('Evolução Rel 12M', format='percent'),
+        'Evolução Rel 24M':st.column_config.NumberColumn('Evolução Rel 24M', format='percent')
+    }
+
+    df_stats = calc_general_stats(df)
+
+    tab_stats, tab_abs, tab_rel = exp3.tabs(tabs=['Dados', 'Histórico de Evolução', 'Crescimento Relativo'])
+
+    with tab_stats:
+        st.dataframe(df_stats, column_config=columns_fmt)
+
+    with tab_abs:
+        abs_cols = [
+            'Diferença Mensal',
+            'Dif Mensal Média 6M',
+            'Dif Mensal Média 12M',
+            'Dif Mensal Média 24M'
+        ]
+        st.line_chart(df_stats[abs_cols])
+    
+    with tab_rel:
+        rel_cols = [
+            'Dif Mensal Relativa',
+            'Evolução Rel 6M',
+            'Evolução Rel 12M',
+            'Evolução Rel 24M'
+        ]
+        st.line_chart(data=df_stats[rel_cols])
---
+++ readme.md
@@ -0,0 +1,5 @@
+streamlit run main.py
+
+atalhos
+    crtl + d (selecionando a cadeia de caracteres que deseja)
+    shift + alt + i (selecionando as linhas que quer, manda o   cursor para o final de todas elas)
